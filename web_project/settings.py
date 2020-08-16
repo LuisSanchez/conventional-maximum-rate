63991ac2f2fdf30c26f79cd565076c0aa58062bb
Luis Sánchez <luis.sanchezm86@gmail.com>
2020-08-16 12:33:13 -0400

    Static content, layout, home page and requirements added.

--- web_project/settings.py
+++ web_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cumplo_api',
+    'external_api',
+    'webapp',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,15 @@
+import re
+from datetime import datetime
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, "webapp/home.html")
+    # return render(
+    #     request,
+    #     'webapp/home.html',
+    #     {
+    #         'name': 'Luis',
+    #         'date': datetime.now()
+    #     }
+    # )
