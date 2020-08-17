242699f904353e916b2f0b8f7ca7fce4d0397842
Luis Sánchez <luis.sanchezm86@gmail.com>
2020-08-17 12:54:44 -0400

    azure support installed

--- web_project/wsgi.py
+++ web_project/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_project.settings')
 
 application = get_wsgi_application()
+
+sys.path.append('/web_project')
+sys.path.append('/webapp')
